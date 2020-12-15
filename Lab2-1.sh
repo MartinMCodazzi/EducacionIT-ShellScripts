c4c8e9566a75ab83f47a005c94dcfeed03280837
MartinMCodazzi <nahuelcodazzi@gmail.com>
2020-12-15 00:12:05 -0300

    .

--- Lab2-1.sh
+++ Lab2-1.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+if [[ $1 -gt $2 ]] && [[ $1 -gt $3 ]] ; then
+        MAYOR= $1
+    elif [[ $2 -gt $3 ]]; then
+        MAYOR=$2
+    else
+        MAYOR=$3
+fi
+
+echo "El Mayor número ingresado es $MAYOR"
