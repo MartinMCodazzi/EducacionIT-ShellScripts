f01b2ce8b6379f7864c5fcbc11726b53d2aeaf57
MartinMCodazzi <nahuelcodazzi@gmail.com>
2020-12-19 10:10:08 -0300

    .

--- Lab2-3.sh
+++ Lab2-3.sh
@@ -3,12 +3,9 @@
 STR1=$1
 STR2=$2
 
-
-
-
 if [[ -z $STR1 ]] || [[ -z $STR2 ]]; then
         echo "Alguna de las cadenas están vacías"
-        
+
   elif [ $STR1 == $STR2 ]; then
         echo "Las cadenas son iguales"
 
